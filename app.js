80324e18205f88abe2a1a8669076a89449267f22
BrightNtt <big_brightstepup@windowslive.com>
2025-06-30 23:15:43 +0700

    add history func and navbar

--- app.js
+++ app.js
@@ -1,23 +1,62 @@
-let plusBtn = document.querySelector('#plusbutton');
+const plusBtn = document.querySelector("#plusbutton");
 let count = 0;
-let display = document.querySelector('#display') 
-let minusBtn = document.querySelector('#minusbutton')
-let resetBtn = document.querySelector('#reset')
+const display = document.querySelector("#display-num");
+const minusBtn = document.querySelector("#minusbutton");
+const resetBtn = document.querySelector("#reset");
+const titleName = document.querySelector("#titlename");
 
-plusBtn.addEventListener('click', function () {
-    count += 1
-    display.innerHTML = count
-})
 
-minusBtn.addEventListener('click', function () {
-    if (count != 0) {
-        count += -1
-    display.innerHTML = count
-    }  
-})
+plusBtn.addEventListener("click", function () {
+  count += 1;
+  display.innerHTML = count;
+});
 
-resetBtn.addEventListener('click', function () {
-    count = 0
-    display.innerHTML = count
+minusBtn.addEventListener("click", function () {
+  if (count != 0) {
+    count += -1;
+    display.innerHTML = count;
+  }
+});
+
+resetBtn.addEventListener("click", function () {
+  count = 0;
+  display.innerHTML = count;
+  clearTitle();
+});
+//finish counting section
+
+titleName.addEventListener("blur", () => {
+  if (titleName.value) {
+    titleName.classList.add("changed");
+  } else {
+    clearTitle();
+  }
+});
+
+function clearTitle() {
+  titleName.value = "";
+  titleName.classList.remove("changed");
+}
+//History section
+const recordBtn = document.querySelector("#record-btn");
+const historyDis = document.querySelector("#history-display");
+
+recordBtn.addEventListener("click", () => {
+  const title = document.createElement("span");
+  title.innerHTML = titleName.value + ":" + " ";
+  historyDis.append(title);
+  const counting = document.createElement("span");
+  counting.innerHTML = count;
+  historyDis.append(counting);
+  const newLine = document.createElement("br");
+  historyDis.append(newLine);
+});
+//history section end
+//navbar start
+const navBarBtn = document.querySelector("#nav-btn");
+const historyContainer = document.querySelector('.navbar-container')
+console.log(historyContainer);
+
+navBarBtn.addEventListener('click', () => {
+    historyContainer.classList.toggle('showed')
 })
-console.log(resetBtn);
